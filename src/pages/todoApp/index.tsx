925764bf1d888d7411950fa8db8de56c80526a30
Saeed Abdi <saeedabdi25.s@gmail.com>
2022-01-09 15:44:03 +0330

    feature 2 has been implemneted

--- src/pages/todoApp/index.tsx
+++ src/pages/todoApp/index.tsx
@@ -15,6 +15,13 @@ const TodoApp = () => {
         },
     ]);
     const { t } = useTranslation();
+
+    const handleDelete = (rowData: TodoType) => {
+        if (window.confirm(t('Are you sure ?'))) {
+            setTodos((prev) => prev.filter((item) => item.id !== rowData.id));
+        }
+    };
+
     return (
         <div className="w-full flex flex-col lg:px-40">
             <AddTodo
@@ -45,17 +52,24 @@ const TodoApp = () => {
                         title: t('Actions'),
                         className: 'flex justify-center',
                         render: (p, rowData) => {
-                            if (rowData?.status) {
-                                return (
-                                    <button className="bg-red-600 rounded-md text-white hover:ring-2 transition-all duration-200 ring-red-600 shadow-md py-2 text-sm">
-                                        {t('Un done')}
-                                    </button>
-                                );
-                            }
                             return (
-                                <button className="bg-blue-600 text-white  hover:ring-2 transition-all duration-200 ring-blue-600  rounded-md shadow-md px-3 py-2 text-sm">
-                                    {t('Un done')}
-                                </button>
+                                <>
+                                    <button
+                                        onClick={() => handleDelete(rowData as TodoType)}
+                                        className="px-3 py-2 mr-2 rounded-md bg-red-600 text-white transition-all hover:ring-2 ring-red-600"
+                                    >
+                                        {t('Delete')}
+                                    </button>
+                                    {!rowData?.status ? (
+                                        <button className="bg-blue-600  rounded-md text-white hover:ring-2 transition-all duration-200 ring--600 shadow-md  py-2 px-3 text-sm">
+                                            {t('Un done')}
+                                        </button>
+                                    ) : (
+                                        <button className="bg-blue-600 text-white  hover:ring-2 transition-all duration-200 ring-green-600  rounded-md shadow-md px-3 py-2 text-sm">
+                                            {t('Un done')}
+                                        </button>
+                                    )}
+                                </>
                             );
                         },
                     },
