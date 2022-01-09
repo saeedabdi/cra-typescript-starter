99bf963a86acedb1bcf5b283c63465446de5119f
Saeed Abdi <saeedabdi25.s@gmail.com>
2022-01-09 12:57:07 +0330

    Eslint prettier tailwind husky and docker is configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
 function App() {
-  return (
-    <div className="w-full min-h-screen bg-red-600">
-      tex
-  
-    </div>
-  );
+    return <div className="w-full min-h-screen bg-red-600">tex</div>;
 }
 
 export default App;
